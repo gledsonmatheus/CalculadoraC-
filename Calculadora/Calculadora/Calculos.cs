1fd0634ad6a58c8d6c210ea149f3589eb61850a0
gledsonmatheus <53982716+gledsonmatheus@users.noreply.github.com>
2021-10-29 18:21:06 -0300

    Primeiro commit.
    projeto para praticar logica e exceções.

---
+++ Calculadora/Calculadora/Calculos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class  Calculos
+    {
+        double resultado = 0;
+
+        public double soma(int a , int b) {
+            resultado = a + b;
+            return resultado;
+        }
+        public double subtracao(int a, int b)
+        {
+            resultado = a - b;
+            return resultado;
+        }
+        public double divisao(int a, int b)
+        {
+            resultado = a / b;
+            return resultado;
+        }
+        public double multiplicao(int a, int b)
+        {
+            resultado = a * b;
+            return resultado;
+        }
+    }
+}
---
+++ Calculadora/Calculadora/TelaInicial.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculadora;
+
+namespace Calculadora
+{
+    public class telainicial
+    {
+        static void Main(string[] args)
+        {
+            Calculos calc = new Calculos();
+            String escolha = "n";
+            int operacao = 0;
+            int primeiroValor = 0;
+            int segundoValor = 0;
+            double resultado = 0;
+
+            Console.WriteLine("deseja fazer alguma operaçao? (S/N) ");
+            escolha = Console.ReadLine();
+
+            if (escolha.ToLower() == "s")
+            {
+                Boolean continuar = true;
+                while (continuar) {
+                    Console.Clear();
+                    try
+                    {
+                        Console.WriteLine("digite o primeiro valor (inteiro)");
+                        primeiroValor = int.Parse(Console.ReadLine());
+                        Console.WriteLine("digite o segundo valor (inteiro)");
+                        segundoValor = int.Parse(Console.ReadLine());
+
+
+                        Console.WriteLine("digite a opção da operaçao desejada  ");
+                        Console.WriteLine("(1) Soma  ");
+                        Console.WriteLine("(2) Subtração  ");
+                        Console.WriteLine("(3) divisão  ");
+                        Console.WriteLine("(4) Multiplicação  ");
+                        operacao = int.Parse(Console.ReadLine());
+
+                        switch (operacao)
+                        {
+                            case 1:
+                                resultado = calc.soma(primeiroValor, segundoValor);
+                                break;
+                            case 2:
+                                resultado = calc.subtracao(primeiroValor, segundoValor);
+                                break;
+                            case 3:
+                                resultado = calc.divisao(primeiroValor, segundoValor);
+                                break;
+                            case 4:
+                                resultado = calc.multiplicao(primeiroValor, segundoValor);
+                                break;
+                            default:
+                                break;
+                        }
+                        Console.WriteLine("O resultado da operação é : " + resultado);
+
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        Console.WriteLine("ouve uma tentativa de dividir por 0, por favor tente novamente e use números validos. ");
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("ocorreu um erro tente novamente");
+                    }
+                    finally
+                    {
+                        Console.WriteLine(" deseja fazer algum calculo (S/N)");
+                        escolha = Console.ReadLine();
+                        if (escolha.ToLower() == "s") { continuar = true; } else { continuar = false; }
+                    }
+
+                }
+               
+
+            }
+            
+                Console.WriteLine("Para encerrar digite qualquer tecla");
+                Console.ReadKey();
+            
+
+        }
+    }
+}
